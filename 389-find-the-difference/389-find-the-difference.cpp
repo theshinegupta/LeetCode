166846b052f9d68808e4f3f988e567eb68d09e6c
Priyanshu Gupta <78649993+priyanshugupta8819@users.noreply.github.com>
2022-09-03 20:03:14 +0530

    Time: 3 ms (79.84%), Space: 6.7 MB (67.38%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            ans=ans^s[i];
+        }
+        
+        for(int i=0;i<t.size();i++)
+        {
+            ans=ans^t[i];
+        }
+        
+        //char res=(char*) ans;
+        
+        return ans;
+    }
+};
