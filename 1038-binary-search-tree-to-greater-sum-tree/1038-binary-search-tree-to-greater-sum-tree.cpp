4bcffeb5294ca3587e5c02765e5b3c9885eaf8a2
Priyanshu Gupta <78649993+priyanshugupta8819@users.noreply.github.com>
2022-09-19 18:35:57 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (55.48%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,54 +11,28 @@
  */
 class Solution {
 public:
-    int hash[10000+1]={0};
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> v;
         
-            
-        inorder(root,v);
-        int idx=v.size();
         
-        hash[v[idx-1]]=v[idx-1];
-        
-        // cout<<hash[v[idx-1]];
-        
-        for(int i=idx-2;i>=0;i--)
-        {
-            hash[v[i]]=v[i]+hash[v[i+1]];
-            // cout<<hash[v[i]]<<" ";
-        }
-        
-        // for(int j=0;j<=10000;j++)
-        // {
-        //     if(hash[j]!=0) cout<<hash[j]<<" "; 
-        // }
-        
-        shineSolve(root);
+        shineTraversal(root);
         
         return root;
-     }
+    }
 private:
-    void inorder(TreeNode* root,vector<int>& v)
-    {
-        if(root==NULL) return;
-        
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
-    }
-    
-    void shineSolve(TreeNode* root)
-    {
-        if(root==NULL) return;
-        
-        shineSolve(root->left);
-        //v.push_back(root->val);
-        root->val= hash[root->val];
-        
-        shineSolve(root->right);
-    }
-    
-    
-
+     int prev=-1;
+     void shineTraversal(TreeNode* root)
+     {
+         if(root==NULL) return;
+         
+         shineTraversal(root->right);
+         
+         if(prev==-1) prev=root->val;
+         else
+         {
+             root->val=root->val+prev;
+             prev=root->val;
+         }
+         
+         shineTraversal(root->left);
+     }
 };
