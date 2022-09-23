8d576a08795a9f384106dbc65730e24ce227a649
Priyanshu Gupta <78649993+priyanshugupta8819@users.noreply.github.com>
2022-09-23 07:51:35 +0530

    Time: 4 ms (37.33%), Space: 7.5 MB (99.77%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -12,15 +12,29 @@ private:
         if(nums.size()==1) return nums[0];
         int maxRob=INT_MIN;
         
-        int dp[100+1]={0};
-            dp[1]=nums[0];
+//         int dp[100+1]={0};
+//             dp[1]=nums[0];
         
         
-        for(int i=2;i<=nums.size();i++)
-        {
-            dp[i]=max((nums[i-1]+dp[i-2]),dp[i-1]);
+        int prev1=nums[0];
+        int prev2=0;
+        
+//         for(int i=2;i<=nums.size();i++)
+//         {
+//             dp[i]=max((nums[i-1]+dp[i-2]),dp[i-1]);
             
-            if(dp[i]>maxRob) maxRob=dp[i];
+//             if(dp[i]>maxRob) maxRob=dp[i];
+//         }
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            int currRob=max((nums[i]+prev2),prev1);
+            if(currRob>maxRob) maxRob=currRob;
+            
+            prev2=prev1;
+            prev1=currRob;
+          
+            
         }
         
         return maxRob;
