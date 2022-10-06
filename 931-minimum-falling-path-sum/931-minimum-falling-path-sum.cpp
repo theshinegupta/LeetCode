a8976d3ac87be8d9a085f20d4c0ef3d8fd1624d4
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2022-10-06 08:54:44 +0530

    Time: 34 ms (28.58%), Space: 10.4 MB (26.67%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -8,18 +8,52 @@ public:
         
         vector<vector<int>> dp(rowLength,vector<int>(columnLength,-1));
         
+        for(int i=0;i<columnLength;i++)
+        {
+            dp[rowLength-1][i]=matrix[rowLength-1][i];
+        }
+        
+        
         int minFall=INT_MAX;
       
-        for(int i=0;i<rowLength;i++)
+       
+        
+       
+        for(int currRow=rowLength-2;currRow>=0;currRow--)
         {
-            int  temp=findMin(0,i,matrix,rowLength,columnLength,dp);
+            for(int currColumn=0;currColumn<columnLength;currColumn++)
+                
+            {
+                
+                 
+                 int movDown=10001, movDLeft=10001, movDRight=10001;
+                
+                  if(currRow+1<rowLength) {
+                                                  movDown=matrix[currRow][currColumn]+findMin(currRow+1,currColumn,matrix,rowLength,columnLength,dp);
+                   if(currColumn-1>=0)            movDLeft=matrix[currRow][currColumn]+findMin(currRow+1,currColumn-1,matrix,rowLength,columnLength,dp);
+                   if(currColumn+1<columnLength)  movDRight=matrix[currRow][currColumn]+findMin(currRow+1,currColumn+1,matrix,rowLength,columnLength,dp);
+                      
+                  dp[currRow][currColumn]=min(min(movDown,movDLeft),movDRight);
+              }
+                
+                
+            }
+                
+        }
+        
+         for(int i=0;i<rowLength;i++)
+        {
+            int  temp=dp[0][i];
             minFall=min(temp,minFall);
         }
+        
         
          
         return minFall;
     }
 private:
+    
+    
     
     int findMin(int currRow,int currColumn,vector<vector<int>>& matrix,int rowLength,int columnLength,vector<vector<int>>& dp)
     {
