ebf806efdc539da00dc04f155ee33e8a22426172
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2022-10-06 07:39:57 +0530

    Time: 28 ms (10.01%), Space: 10.2 MB (31.28%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -7,7 +7,26 @@ public:
         
         
         vector<vector<int>> dp(m,vector<int> (n,0));
-        return minCost(0,0,m,n,dp,grid);
+       
+        dp[0][0]=grid[0][0];
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               
+                if(i==0 && j==0) continue;
+                
+                int right=10000001, down=10000001;
+                
+                if(j-1>=0) right=grid[i][j]+dp[i][j-1];
+                if(i-1>=0) down=grid[i][j] + dp[i-1][j];
+                    
+                dp[i][j]=min(right,down);
+            }
+        }
+        
+        return dp[m-1][n-1];
         
     }
 
@@ -16,10 +35,10 @@ private:
     int minCost(int currRow,int currColumn,int& m,int& n, vector<vector<int>>& dp,vector<vector<int>>& grid)
     {
         
-        if(currRow>=m || currColumn>=n)
+        if(currRow<0 || currColumn<0)
             return 100001;
         
-        if(currRow==(m-1) && currColumn==(n-1))
+        if(currRow==(0) && currColumn==(0))
            return grid[currRow][currColumn];
         
         
@@ -27,8 +46,8 @@ private:
         
        if(dp[currRow][currColumn]!=0) return dp[currRow][currColumn];
         
-        int  rightMove=grid[currRow][currColumn] +minCost(currRow,currColumn+1,m,n,dp,grid);
-        int downMove=grid[currRow][currColumn]+ minCost(currRow+1,currColumn,m,n,dp,grid);
+        int  rightMove=grid[currRow][currColumn] +minCost(currRow,currColumn-1,m,n,dp,grid);
+        int downMove=grid[currRow][currColumn]+ minCost(currRow-1,currColumn,m,n,dp,grid);
         
         
         dp[currRow][currColumn]=min(rightMove,downMove);
