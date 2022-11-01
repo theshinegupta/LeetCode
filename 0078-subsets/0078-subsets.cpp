8bf6795aa542e28e36112abb596eeadc3decf72a
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2022-11-01 22:35:08 +0530

    Time: 9 ms (22.34%), Space: 16.1 MB (22.54%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int> v;
+        
+        gnSubset(0,nums,v);
+        
+        return ans;
+        
+    }
+void gnSubset(int idx,vector<int> &nums,vector<int> v)
+{
+    if(idx>=nums.size())
+    {ans.push_back(v); return;}
+    
+    
+    gnSubset(idx+1,nums,v);
+    v.push_back(nums[idx]);
+    
+    gnSubset(idx+1,nums,v);
+    
+    
+    
+    
+    
+}
+};
