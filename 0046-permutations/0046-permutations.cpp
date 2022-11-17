3e7a312791968d01eb47c5ffe6c2a43dab362613
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2022-11-17 12:10:07 +0530

    Time: 4 ms (68.34%), Space: 7.8 MB (47.67%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> v;
+        
+        shineSolve(0,v,nums);
+        
+        // swap(nums[1],nums[2]);
+        // for(int i=0;i<nums.size();i++)
+        //     cout<<nums[i]<<" ";
+        
+        v.push_back(nums);
+        
+        return v;
+        
+    }
+    
+void shineSolve(int currPos,vector<vector<int>>&v ,vector<int> currConfig)
+{
+    if(currPos>=currConfig.size()-1)
+    {
+        return;
+    }
+    
+    for(int i=currPos;i<currConfig.size();i++)
+    {
+        for(int j=i+1;j<currConfig.size();j++)
+        {
+            swap(currConfig[i],currConfig[j]);
+            v.push_back(currConfig);
+            
+            shineSolve(i+1,v,currConfig);
+            swap(currConfig[i],currConfig[j]);
+            
+        }
+    }
+}
+};
