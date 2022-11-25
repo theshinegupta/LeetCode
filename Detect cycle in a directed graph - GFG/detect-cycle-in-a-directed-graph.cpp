d1516958159586109afc9148cc2d524349b11f0e
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2022-11-25 21:20:23 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -8,64 +8,69 @@ class Solution {
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         bool isVisited[V];
-        unordered_map<int,int> mp;
-        bool flag=false;
-        
         memset(isVisited,false,sizeof(isVisited));
+        
+        
+        int inDegree[V];
+        
+        memset(inDegree,0,sizeof(inDegree));
         
         for(int i=0;i<V;i++)
         {
-            if(!isVisited[i])
+            for(int j=0;j<adj[i].size();j++)
             {
-                dfs(i,adj,flag,isVisited,mp);
-                
-                if(flag)
-                {
-                   // cout<<"hi"<<" ";
-                    return true;
-                }
-                
-               //cout<<
-                 
+                inDegree[adj[i][j]]+=1;
             }
         }
         
         
+        queue<int> q;
+        
+        for(int i=0;i<V;i++)
+        {
+            if(inDegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty())
+        {
+            int currVrtx=q.front();
+            q.pop();
+            
+            if(!isVisited[currVrtx])
+            {
+                isVisited[currVrtx]=true;
+                for(int i=0;i<adj[currVrtx].size();i++)
+                {
+                    inDegree[adj[currVrtx][i]]-=1;
+                    
+                    if(inDegree[adj[currVrtx][i]]==0)
+                    {
+                        q.push(adj[currVrtx][i]);
+                    }
+                }
+            }
+        }
+        
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!isVisited[i])
+              {
+                  
+                  //cout<<i<<" ";
+                  return true;
+              }
+              
+              
+        }
+        
         return false;
-    }
-    
-void dfs(int currVrtx,vector<int> adj[],bool& flag,bool isVisited[],unordered_map<int,int>& mp)
-{
-    if(isVisited[currVrtx] && mp.find(currVrtx)!=mp.end())
-     {
-         
-         flag=true;
-         
-        // cout<<flag<<" ";
-         return;
-     }
-     
-    if(isVisited[currVrtx])
-      return;
-      
-    
-    isVisited[currVrtx]=true;
-    mp[currVrtx]++;
-    for(int i=0;i<adj[currVrtx].size();i++)
-    {
-        dfs(adj[currVrtx][i],adj,flag,isVisited,mp);
         
         
     }
-    
-    mp[currVrtx]--;
-    if(mp[currVrtx]==0)
-       mp.erase(currVrtx);
-       
-      
-    
-    
-}
 };
 
 //{ Driver Code Starts.
