c2c3a9d9b8f6d5470aeaf1302e7986b47c306b2f
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2023-01-02 12:08:44 +0530

    Added solution - LeetHub

---
+++ Leaf at same level - GFG/leaf-at-same-level.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/* The structure of the binary tree is as follows
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+class Solution{
+  public:
+    int setLvl=-1;
+    bool check(Node *root)
+    {
+        
+        
+        if(root->left==NULL && root->right==NULL)
+          return true;
+          
+        
+        bool flag=true;
+        traverse(root,0,flag);
+       
+       
+    //   cout<<setLvl<<"\n"; 
+        return flag;
+    }
+    
+void traverse(Node* root,int currLvl,bool& flag)
+{
+    if(root==NULL)
+      return;
+      
+    if(root->left==NULL && root->right==NULL)
+    {
+        if(setLvl==-1)
+           {
+               setLvl=currLvl;
+               return;
+           }
+        
+        if(currLvl!=setLvl)
+        {
+            flag=false;
+        }
+         
+        return;
+    }
+    
+    // if(!flag)
+    //   return;
+       
+    traverse(root->left,currLvl+1,flag);
+    traverse(root->right,currLvl+1,flag);
+}
+};
+
+//{ Driver Code Starts.
+// Driver program to test size function
+int main()
+{
+    int t;
+    cin>> t;
+    getchar();
+    while (t--)
+    {
+        string s;
+        getline( cin, s );
+        Node* head = buildTree( s );
+        Solution obj;
+        cout << obj.check(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
