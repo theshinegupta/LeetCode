37032bbc80e6866ae6aba711aa89a985259e713d
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2023-03-20 08:09:27 +0530

    Time: 243 ms (25.59%), Space: 163 MB (10.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
+    vector<vector<string>> res;
     vector<vector<string>> partition(string s) {
+        
         vector<string> v;
         shineSolve(s,0,s.length()-1,v);
-        return ans;
+        
+        return res;
+        
     }
     
-    void shineSolve(string&s,int i,int j,vector<string>& v)
+void shineSolve(string& s ,int start,int end,vector<string> currStr)
+{
+    if(start>end)
     {
-        if(i>j || i>=s.length())
-        {
-            ans.push_back(v);
-            return;
-        }
-            
-        if(i==j)
-        {
-            string temp2="";
-            temp2+=s[i];
-            v.push_back(temp2);
-            ans.push_back(v);
-             v.pop_back();
-            return;
-        }
-        
-        for(int k=i;k<=j;k++)
-        {
-            string temp=s.substr(i,k-i+1);
-            
-            if(isPalindrome(temp))
-            {
-                v.push_back(temp);
-                shineSolve(s,k+1,j,v);
-                 v.pop_back();
-            }
-        }
+        // cout<<"hi ";
+        res.emplace_back(currStr);
         return;
     }
     
-    bool isPalindrome(string& str)
+    for(int i=1;i<=end-start+1;i++)
     {
-        int i=0;
-        int j=str.length()-1;
+        string temp=s.substr(start,i);
         
-        while(i<j)
+        if(isPal(temp))
         {
-            if(str[i]!=str[j])
-                return false;
-            i++;
-            j--;
+            // cout<<"he"<<"";
+            currStr.emplace_back(temp);
+            shineSolve(s,start+i,end,currStr);
+              currStr.pop_back();
         }
-        
-        return true;
     }
     
+    
+   
+}
+    
+bool isPal(string& temp)
+{
+    int start=0;
+    int end=temp.length()-1;
+    
+    while(start<end)
+    {
+        if(temp[start]!=temp[end])
+            return false;
+        
+        start++;
+        end--;
+    }
+    
+    
+    return true;
+}
 };
